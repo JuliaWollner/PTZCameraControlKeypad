af36f43b931d9599ad45827e3cac46b419ff859c
JuliaWollner <julia_ez2000@yahoo.de>
2020-04-13 10:08:13 +0200

    import stream

---
+++ stream/streamhandler.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Import der notwendigen Bibliotheken
+import os
+import json
+import time
+import signal
+import socket
+import subprocess as sub
+
+# Zugangsdaten importieren
+with open("YOURS") as creds:
+	credentials = json.load(creds)
+
+# Zuweisen der Logindaten
+username = credentials['benutzername']
+password = credentials['passwort']
+server = credentials['server']
+rtspport = credentials['rtspport']
+httpport = credentials['httpport']
+
+# Hilfsvariablen erstellen
+programm = 'vlc'
+pid = None
+online = None
+quality = '11'
+
+# Funktion zum prüfen ob Kamera erreichbar
+def servercheck(ip,httpport):
+	try:
+		s = socket.create_connection((ip,httpport),0.1)
+		s.close()
+		return True
+	except:
+		return False
+
+# Hauptprogramm
+while True:
+	online = servercheck(server,int(httpport))
+	try:
+		programm = programm[:15]
+		pid = sub.check_output("pgrep %s" %programm, shell=True)
+		if online == False:
+			pid = int(pid)
+			os.kill(pid, signal.SIGKILL)
+		else:
+			pass
+	except:
+		if online == True:
+			sub.call (["DISPLAY=:0 vlc rtsp://%s:%s@%s:%s/%s --quiet --rtsp-tcp --noaudio --fullscreen --no-qt-privacy-ask --no-video-title --network-caching=500 --sout-mux-caching=500 vlc://quit &" %(username,password,server,rtspport,quality)], shell=True)
+		else:
+			pass
+	time.sleep(1)
